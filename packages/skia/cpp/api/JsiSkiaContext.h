8012f88485b055419891ad9d8afefb64113792d7
Terry Sahaidak <tasssik@gmail.com>
2024-10-08 16:04:49 +0300

    wip graphite init

--- packages/skia/cpp/api/JsiSkiaContext.h
+++ packages/skia/cpp/api/JsiSkiaContext.h
@@ -21,12 +21,22 @@
 
 #include "include/core/SkFont.h"
 #include "include/core/SkFontMetrics.h"
+#include "include/core/SkColorType.h"
+#include "include/gpu/graphite/dawn/DawnTypes.h"
+#include "include/gpu/graphite/dawn/DawnUtils.h"
 #include "include/gpu/graphite/Context.h"
 #include "include/gpu/graphite/Recorder.h"
 #include "include/gpu/graphite/Recording.h"
-#include "include/gpu/graphite/Surface.h"
 #include "include/gpu/graphite/dawn/DawnBackendContext.h"
-#include <GPUAdapter.h>
+#include "include/gpu/graphite/BackendTexture.h"
+#include "include/gpu/graphite/Surface.h"
+
+#include <GPUDevice.h>
+#include <GPU.h>
+#include "Convertors.h"
+#include "GPUCanvasContext.h"
+#include <RNFJSIConverter.h>
+
 
 #pragma clang diagnostic pop
 
@@ -39,7 +49,79 @@ public:
   EXPORT_JSI_API_TYPENAME(JsiSkiaContext, SkiaContext)
 
   JSI_HOST_FUNCTION(createGraphite) {
-    return jsi::undefined();
+    auto gpuContext = margelo::JSIConverter<std::shared_ptr<rnwgpu::GPUCanvasContext>>::fromJSI(runtime, arguments[0], false);
+    auto gpu = margelo::JSIConverter<std::shared_ptr<rnwgpu::GPU>>::fromJSI(
+            runtime, arguments[1], false);
+    auto device = margelo::JSIConverter<std::shared_ptr<rnwgpu::GPUDevice>>::fromJSI(
+            runtime, arguments[2], false);
+
+    auto queue = device->getQueue();
+
+     skgpu::graphite::DawnBackendContext dawnContext;
+
+     dawnContext.fInstance = gpu->get();
+     dawnContext.fDevice = device->get();
+     dawnContext.fQueue = queue->get();
+
+     skgpu::graphite::ContextOptions options;
+
+      // Create a Skia Graphite context for Dawn
+    std::shared_ptr<skgpu::graphite::Context> skiaContext = skgpu::graphite::ContextFactory::MakeDawn(dawnContext, options);
+
+
+    if (!skiaContext) {
+        printf("Failed to create Skia Graphite Context with Dawn\n");
+        return -1;
+    }
+
+    std::unique_ptr<skgpu::graphite::Recorder> recorder = skiaContext->makeRecorder();
+
+    auto texture = gpuContext->getCurrentTexture();
+
+    skgpu::graphite::DawnTextureInfo info(/*sampleCount=*/1,
+                                          skgpu::Mipmapped::kNo,
+                                          texture->get().GetFormat(),
+                                          texture->get().GetUsage(),
+                                          wgpu::TextureAspect::All);
+
+    auto backendTex = skgpu::graphite::BackendTextures::MakeDawn(texture->get().Get());
+
+    SkSurfaceProps props(0, kUnknown_SkPixelGeometry);
+
+//     Create Skia Graphite surface
+    auto surface = SkSurfaces::WrapBackendTexture(recorder.get(),
+                                                  backendTex,
+                                                  kBGRA_8888_SkColorType,
+                                                  nullptr,
+                                                  &props);
+    if (!surface) {
+        printf("Failed to create Skia Surface with Graphite\n");
+        return -1;
+    }
+
+    SkCanvas* canvas = surface->getCanvas();
+
+    if (canvas) {
+        // Clear the canvas with a color
+        canvas->clear(SK_ColorWHITE);
+
+        // Draw a simple rectangle
+        SkPaint paint;
+        paint.setColor(SK_ColorRED);
+        canvas->drawRect(SkRect::MakeWH(100, 100), paint);
+    }
+
+    auto recording = recorder->snap();
+    if (recording) {
+     skiaContext->insertRecording({recording.get()});
+    }
+
+    skiaContext->submit();
+
+    gpuContext->present();
+
+
+    return jsi::Value::undefined();
   }
 
   JSI_HOST_FUNCTION(getSurface) {
@@ -77,11 +159,6 @@ public:
       void *surface = reinterpret_cast<void *>(nativeBufferPointer);
       auto width = static_cast<int>(arguments[1].asNumber());
       auto height = static_cast<int>(arguments[2].asNumber());
-
-
-      
-      skgpu::graphite::DawnBackendContext dawnContext;
-      
       
       auto result =
           context->makeContextFromNativeSurface(surface, width, height);
